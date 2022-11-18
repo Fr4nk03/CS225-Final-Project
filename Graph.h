a98bdd65ccb8eef3794935b7806b3b62c08d8272
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2022-11-18 17:59:29 -0600

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Edge {
+    string label;
+    Vertex dest;
+    int weight;
+};
+
 //node for adjacency list 
-struct node {
+struct Vertex {
+    string label;
     int val, pageRank;
-    node *next;
-}
+    vector<Edge> edges;
+};
+
+class Graph {
+    public:
+        void addVertex(Vertex v);
+        void addEdge(Vertex from, Vertex to, string label, int weight);
+    private:
+        vector<Vertex> vertices;
+};

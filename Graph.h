21741473883e9f35942969644dbd72e97a3b8d79
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2022-11-18 18:05:26 -0600

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 struct Edge {
+    Edge(string label, Vertex dest, int weight) : label(label), dest(dest), weight(weight) {}
     string label;
     Vertex dest;
     int weight;
@@ -13,13 +15,13 @@ struct Edge {
 struct Vertex {
     string label;
     int val, pageRank;
-    vector<Edge> edges;
 };
 
 class Graph {
     public:
+        Graph() = default;
         void addVertex(Vertex v);
         void addEdge(Vertex from, Vertex to, string label, int weight);
     private:
-        vector<Vertex> vertices;
+        unordered_map<Vertex, vector<Edge>> vertices;
 };

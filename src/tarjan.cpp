c79a3a1ac38485bff697aa0d9676929a29698bf8
AnderZhu <shuqinz2@illinois.edu>
2022-11-29 01:11:17 +0000

    update

--- src/tarjan.cpp
+++ src/tarjan.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 vector<int> findSCCs(map<Product, vector<Edge>> graph) {
     stack<Product> stack;
+    vector<bool> visited(graph.size(), false);
+    vector<int> lowLink(graph.size(), 0);
 }
 
 
@@ -13,73 +15,73 @@ vector<int> findSCCs(map<Product, vector<Edge>> graph) {
 
 
 
-/*private member
+// /*private member
 
-two
-1 number of nodes n
-2 adjacency list
+// two
+// 1 number of nodes n
+// 2 adjacency list
 
-id and sccCount to give name and count number
+// id and sccCount to give name and count number
 
-id = []
-visited = []
-low = []
+// id = []
+// visited = []
+// low = []
 
-onStack[false]
-stack an empty stack
+// onStack[false]
+// stack an empty stack
 
-*/
+// */
 
-/*
-std::vector<int> nodes
-std::vector<std::vector<int>> adjList
- std:vector<int> low;
-std:vector<bool> visited;
+// /*
+// std::vector<int> nodes
+// std::vector<std::vector<int>> adjList
+//  std:vector<int> low;
+// std:vector<bool> visited;
 
-std:vector<bool> onStack;
+// std:vector<bool> onStack;
 
-*/
+// */
 
 
-void findScc(std::vector<int> nodes, std::vector<std::vector<int>> adjList) {
-    std:vector<bool> visited;
-    visited.resize(nodes.size, false);
-    std:vector<bool> onStack;
-    onStack.resize(nodes.size, false);
-    std:vector<int> low;
-    for (int i = 0; i < nodes.size(); i++) {
-        if (!visited[i]) {
-          dfs(i);
-        }
-    }
-}
-void dfs(int at) {
-      std::stack<int> stack;
-      std:vector<bool> onStack;
-      onStack.resize(nodes.size, false);
+// void findScc(std::vector<int> nodes, std::vector<std::vector<int>> adjList) {
+//     std:vector<bool> visited;
+//     visited.resize(nodes.size, false);
+//     std:vector<bool> onStack;
+//     onStack.resize(nodes.size, false);
+//     std:vector<int> low;
+//     for (int i = 0; i < nodes.size(); i++) {
+//         if (!visited[i]) {
+//           dfs(i);
+//         }
+//     }
+// }
+// void dfs(int at) {
+//       std::stack<int> stack;
+//       std:vector<bool> onStack;
+//       onStack.resize(nodes.size, false);
       
 
-      stack.push(at);
-      onStack[at] = true;
+//       stack.push(at);
+//       onStack[at] = true;
 
-      for (auto to : adjList) {
-        if (visited[to] == false) {
-            dfs(to);
-        }
-        if (onStack[to]) {
-            low[onStack] = min(low[at], low[to]);
-        }
+//       for (auto to : adjList) {
+//         if (visited[to] == false) {
+//             dfs(to);
+//         }
+//         if (onStack[to]) {
+//             low[onStack] = min(low[at], low[to]);
+//         }
 
-      }
-      ///check for start
-      if (nodes[at] == low[at]) {
-        for (node = stack.pop(); ;node = stack.pop()) {
-            onStack[node] = false;
-            low[node] = nodes[at];
-            if (node = at) {
-                break;
-            }
-        }
-      }
+//       }
+//       ///check for start
+//       if (nodes[at] == low[at]) {
+//         for (node = stack.pop(); ;node = stack.pop()) {
+//             onStack[node] = false;
+//             low[node] = nodes[at];
+//             if (node = at) {
+//                 break;
+//             }
+//         }
+//       }
 
-}
+// }

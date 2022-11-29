cfbe4262705aad5b313c466d14bd8671e861d95d
AnderZhu <shuqinz2@illinois.edu>
2022-11-29 00:21:07 +0000

    deleted val

--- src/Graph.h
+++ src/Graph.h
@@ -9,13 +9,13 @@ using namespace std;
 
 // Product node for adjacency list 
 struct Product {
-    Product(string label, int val, int pageRank) : label(label), val(val), pageRank(pageRank) {};
-    string label;
-    int val, pageRank;
+    Product(int label, int val, int pageRank) : label(label), pageRank(pageRank) {};
+    int label;
+    int pageRank;
 
     // Comparison function for unordered_map
     bool operator==(const Product& p) const {
-        return label == p.label && val == p.val && pageRank == p.pageRank;
+        return label == p.label && pageRank == p.pageRank;
     }
 };
 
@@ -24,7 +24,7 @@ class MyHashFunction {
     public:
         size_t operator()(const Product& p) const
         {
-            return p.label.length() / p.val;
+            return p.label;
         }
 };
 

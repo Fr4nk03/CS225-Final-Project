1d4fa3ebdd242eceb63421837ea098e2152694a4
AnderZhu <shuqinz2@illinois.edu>
2022-11-29 00:45:56 +0000

    update

--- src/tarjan.cpp
+++ src/tarjan.cpp
@@ -1,5 +1,9 @@
 #include "tarjan.h"
 
-std::vector<int> findSCCs(Graph g) {
+using namespace std;
 
+vector<int> findSCCs(Graph g) {
+    unordered_map<Product, bool, MyHashFunction> visited;
+    unordered_map<Product, int, MyHashFunction> lowLink;
+    stack<Product> stack;
 }

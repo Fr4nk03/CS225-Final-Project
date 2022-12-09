3a6f1f4f24136abdc9be11c675d5fdbf5f10c83a
picolimpid <92841510+picolimpid@users.noreply.github.com>
2022-12-08 20:58:45 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -50,26 +50,37 @@ TEST_CASE("tarjan Public Test 2", "[weight=5]") {
     REQUIRE( findSCCs(g)== v);
 }
 TEST_CASE("tarjan Public Test 3", "[weight=8]") {
-   Product v1; Product v2; Product v3;
+   Product v1("v1"); Product v2("v2"); Product v3("v3");
+   Product v4("v4"); Product v5("v5"); Product v6("v6");
+   Product v7("v7"); Product v8("v8");
    Graph g;
     g.addProduct(v1); g.addProduct(v2); g.addProduct(v3);
    g.addProduct(v4); g.addProduct(v5); g.addProduct(v6);
     g.addProduct(v7); g.addProduct(v8);
-   addEdge(v1, v2, 1);
-   addEdge(v3, v1, 1);
-   addEdge(v2, v3, 1);
-    addEdge(v4, v2, 1);
-   addEdge(v4, v3, 1);
-   addEdge(v4, v5, 1);
-   addEdge(v5, v4, 1);
-   addEdge(v5, v6, 1);
-   addEdge(v6, v3, 1);
-   addEdge(v6, v7, 1);
-   addEdge(v7, v6, 1);
-   addEdge(v8, v5, 1);
-   addEdge(v8, v8, 1);
-   addEdge(v8, v7, 1);
-    REQUIRE( findSCCs(g) == {1,1,1,4,4,6,6,8});
+   g.addEdge(v1, v2, 1);
+   g.addEdge(v3, v1, 1);
+   g.addEdge(v2, v3, 1);
+    g.addEdge(v4, v2, 1);
+   g.addEdge(v4, v3, 1);
+   g.addEdge(v4, v5, 1);
+   g.addEdge(v5, v4, 1);
+   g.addEdge(v5, v6, 1);
+   g.addEdge(v6, v3, 1);
+   g.addEdge(v6, v7, 1);
+   g.addEdge(v7, v6, 1);
+   g.addEdge(v8, v5, 1);
+   g.addEdge(v8, v8, 1);
+   g.addEdge(v8, v7, 1);
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(4);
+    v.push_back(4);
+    v.push_back(6);
+    v.push_back(6);
+    v.push_back(8);
+    REQUIRE( findSCCs(g) == v);
 
   
 

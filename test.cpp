fc2363378af216743f0dd760b543df8c98c87db3
picolimpid <92841510+picolimpid@users.noreply.github.com>
2022-12-08 21:15:47 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -71,15 +71,15 @@ TEST_CASE("tarjan Public Test 3", "[weight=8]") {
    g.addEdge(v8, v5, 1);
    g.addEdge(v8, v8, 1);
    g.addEdge(v8, v7, 1);
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(4);
-    v.push_back(4);
-    v.push_back(6);
-    v.push_back(6);
-    v.push_back(8);
+   vector<int> v;
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(3);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(7);
     REQUIRE( findSCCs(g) == v);
 
   
